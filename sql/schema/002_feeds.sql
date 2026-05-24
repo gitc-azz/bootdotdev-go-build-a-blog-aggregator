b6ba662393f04c2ccc5a01f6c65e2640704d7f6e
Azz <graphical.it.consulting@proton.me>
2026-05-24 12:40:25 +0200

    fix: wrongly implemented many-to-many replaced by a one-to-many relation between users and feeds tables

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -4,9 +4,8 @@ CREATE TABLE feeds (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL,
-    url TEXT NOT NULL,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    UNIQUE (url, user_id)
+    url TEXT UNIQUE NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
